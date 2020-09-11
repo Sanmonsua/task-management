8c9005f6ead62f8e2c9d854678187f30cdf2eb77
Sanmonsua <sanmonsua@gmail.com>
2020-09-10 21:38:14 -0500

    Add : BackButton component

---
+++ components/BackButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { TouchableOpacity } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'; 
+
+
+export default function BackButton({ onPress }) {
+    return (
+        <TouchableOpacity onPress={onPress}>
+            <Ionicons name="ios-arrow-round-back" size={50} color="black" />
+        </TouchableOpacity>
+    )
+}
