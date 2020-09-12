c05830ef1d3cb3a352a447961336e5429278fd80
Sanmonsua <sanmonsua@gmail.com>
2020-09-11 21:53:31 -0500

    Add : blank checkbox pressable icon for tasks not done

--- components/Task.js
+++ components/Task.js
@@ -1,20 +1,22 @@
 import React from 'react'
-import {TouchableOpacity, Text, StyleSheet, View} from 'react-native'
-import { Ionicons } from '@expo/vector-icons'; 
+import { TouchableOpacity, Text, StyleSheet, View } from 'react-native'
+import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons'; 
 
 export const Task = ({item, color, onPress}) => (
     <TouchableOpacity 
-        onPress={onPress} 
         style={styles.taskContainer}
     >
         <View style={{flexDirection: 'row', justifyContent:'space-between'}}>
             <Text style={styles.taskName}>
                 {item.name}
             </Text>
+            <TouchableOpacity onPress={onPress}>
+                {item.done 
+                    ? <Ionicons name="ios-checkbox" size={24} color={color} />
+                    : <MaterialCommunityIcons name="checkbox-blank-outline" size={24} color="#d1d9e4" />
+                }
+            </TouchableOpacity>
             
-            {item.done && 
-                <Ionicons name="ios-checkbox" size={24} color={color} />
-            }
             
         </View>
         
@@ -22,10 +24,11 @@ export const Task = ({item, color, onPress}) => (
             <Text style={styles.taskDate}>
                 {item.date.toUpperCase()}
             </Text>
-            {item.done && 
-                <Text style={{... styles.taskDate, color:color}}>
+            {item.done 
+                ? <Text style={{... styles.taskDate, color:color}}>
                     COMPLETED !
                 </Text>
+                : <Text style={styles.taskDate}>TO-DO</Text>
             }
             
         </View>
